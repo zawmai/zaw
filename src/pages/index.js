2c96f64f0651daae7c7505a3b9190d060ae7f093
Zaw Mai <z.mai@fi.com>
2021-07-09 22:51:54 -0700

    Added StaticImage component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Zaw Mai - Home Page">
-       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Wall wraffiti of a cat popping out its head from the right side of the wall and saying 'hello'"
+        src="https://source.unsplash.com/qHMf-I-JAXo"
+      />
+      <StaticImage
+        alt="Selfie shot of Zaw Mai at Washington Park in Portland, Oregon"
+        src="../images/selfie.jpg"
+      />
     </Layout>
   );
 }
