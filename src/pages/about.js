fbd3e696b6e8dfb27ca93b6314a7cd4d33a4def4
Zaw Mai <z.mai@fi.com>
2021-07-09 19:03:38 -0700

    Add home/demos/about pages, layout block, layout css

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="Zaw Mai - About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with gatsby</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/demos.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout'
+
+const DemosPage = () => {
+  return (
+    <Layout pageTitle="Check Out My Demos">
+      <p>My Demos goes here!</p>
+    </Layout>
+  );
+};
+
+export default DemosPage;
