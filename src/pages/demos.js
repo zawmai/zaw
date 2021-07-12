a6fc33cc5176246ead4bf7aabf8c8016b8329903
Zaw Mai <z.mai@fi.com>
2021-07-11 23:01:35 -0700

    Updated Demo Page with first project link

--- src/pages/demos.js
+++ src/pages/demos.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import Layout from '../components/layout'
+import Layout from '../components/layout';
+import navLinkText from '../components/navbar.module.css';
 
 const DemosPage = () => {
   return (
     <Layout pageTitle="Check Out My Demos">
-      <p>My Demos goes here!</p>
+      <p><a href="//demos.zawmai.com/wallbox" targetclassName={navLinkText}>Wallbox</a> - EV Charger E-commerce Store Homepage</p>
+      <p></p>
     </Layout>
   );
 };
