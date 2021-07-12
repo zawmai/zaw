f3ded52d2715a5ec8ee6f56e39f1bea95ddd62e0
Zaw Mai <z.mai@fi.com>
2021-07-11 23:21:40 -0700

    Updated demo page text

--- src/pages/demos.js
+++ src/pages/demos.js
@@ -5,7 +5,11 @@ import navLinkText from '../components/navbar.module.css';
 const DemosPage = () => {
   return (
     <Layout pageTitle="Check Out My Demos">
-      <p><a href="//demos.zawmai.com/wallbox" targetclassName={navLinkText}>Wallbox</a> - EV Charger E-commerce Store Homepage</p>
+      <p>
+        <a href="//demos.zawmai.com/wallbox" targetclassName={navLinkText}>Wallbox</a> - EV Charger E-commerce Store Homepage
+        <br/>
+        <strong>Tech Used</strong>: Bootstrap, SwiperJS, Node, Nginx, Express
+      </p>
       <p></p>
     </Layout>
   );
